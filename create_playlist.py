df689deaa824c6955d7e376be7cf9079b2bdc24f
amnindersingh12 <amnindersingh58611@gmail.com>
2021-08-15 11:25:48 +0530

    adding sauce

---
+++ create_playlist.py
@@ -0,0 +1,21 @@
+"""
+Step 1: Log Into Youtube
+Step 2: Grab Our Playlist SOngs
+Step 3: Create a New Playlist
+Step 4: Search For the song
+Step 5: Add this song into the new YT playlist
+"""
+
+class CreatePlaylist:
+    def __init__(self):
+        pass
+    def get_youtube_client(self):
+        pass
+    def get_liked_videos(self):
+        pass
+    def create_playlist(self):
+        pass
+    def get_spotify_url(self):
+        pass
+    def add_song_to_playlist(self):
+        pass
